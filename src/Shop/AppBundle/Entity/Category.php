dbc14e584cbcc2d9b98114a22ffe3dac75adb5bc
krzysiek <k.spetkowski@wp.pl>
2015-11-11 18:31:01 +0100

    git.ignore

---
+++ src/Shop/AppBundle/Entity/Category.php
@@ -0,0 +1,94 @@
+<?php
+
+namespace Shop\AppBundle\Entity;
+
+use Doctrine\ORM\Mapping as ORM;
+
+/**
+ * Category
+ *
+ * @ORM\Table()
+ * @ORM\Entity
+ */
+class Category
+{
+    /**
+     * @var integer
+     *
+     * @ORM\Column(name="id", type="integer")
+     * @ORM\Id
+     * @ORM\GeneratedValue(strategy="AUTO")
+     */
+    private $id;
+
+    /**
+     * @var string
+     *
+     * @ORM\Column(name="name", type="string", length=255)
+     */
+    private $name;
+
+    /**
+     * @var string
+     *
+     * @ORM\Column(name="product", type="string", length=255)
+     */
+    private $product;
+
+
+    /**
+     * Get id
+     *
+     * @return integer 
+     */
+    public function getId()
+    {
+        return $this->id;
+    }
+
+    /**
+     * Set name
+     *
+     * @param string $name
+     * @return Category
+     */
+    public function setName($name)
+    {
+        $this->name = $name;
+
+        return $this;
+    }
+
+    /**
+     * Get name
+     *
+     * @return string 
+     */
+    public function getName()
+    {
+        return $this->name;
+    }
+
+    /**
+     * Set product
+     *
+     * @param string $product
+     * @return Category
+     */
+    public function setProduct($product)
+    {
+        $this->product = $product;
+
+        return $this;
+    }
+
+    /**
+     * Get product
+     *
+     * @return string 
+     */
+    public function getProduct()
+    {
+        return $this->product;
+    }
+}
---
+++ src/Shop/AppBundle/ShopAppBundle.php
@@ -0,0 +1,9 @@
+<?php
+
+namespace Shop\AppBundle;
+
+use Symfony\Component\HttpKernel\Bundle\Bundle;
+
+class ShopAppBundle extends Bundle
+{
+}
